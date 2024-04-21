6424a6a42bd8b43fc2ba01152112f8312efa246e
idnoble33 <id.bellodev@gmail.com>
2024-04-20 18:56:28 -0600

    update the key name

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -15,7 +15,7 @@ data "aws_ami" "amazon-linux" {
 resource "aws_instance" "dev_machine" {
   ami = data.aws_ami.amazon-linux.id
   instance_type = "t2.micro"
-  key_name = "k8"
+  key_name = "idnoble-jenkins"
 
   tags = {
     Environment = "dev"
