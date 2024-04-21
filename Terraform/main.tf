89e67ec54eda508f52efb5df205925c34b8d817e
idnoble33 <id.bellodev@gmail.com>
2024-04-20 19:42:34 -0600

    remove key

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -15,7 +15,6 @@ data "aws_ami" "amazon-linux" {
 resource "aws_instance" "dev_machine" {
   ami = data.aws_ami.amazon-linux.id
   instance_type = "t2.micro"
-  key_name = "ansible"
 
   tags = {
     Environment = "dev"
